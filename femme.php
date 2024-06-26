266f07ac8f6e83d8419cd25d2c6519b35bba5831
saifmogaadi12 <aboura.joujou@outlook.es>
2024-06-26 19:53:52 +0100

    product page

--- femme.php
+++ femme.php
@@ -73,18 +73,18 @@
               xml:space="preserve" class="u-svg-content" viewBox="0 0 53 53" x="0px" y="0px" id="svg-13a8"
               style="enable-background: new 0 0 53 53">
               <path style="fill: #e7eced" d="M18.613,41.552l-7.907,4.313c-0.464,0.253-0.881,0.564-1.269,0.903C14.047,50.655,19.998,53,26.5,53
-	c6.454,0,12.367-2.31,16.964-6.144c-0.424-0.358-0.884-0.68-1.394-0.934l-8.467-4.233c-1.094-0.547-1.785-1.665-1.785-2.888v-3.322
-	c0.238-0.271,0.51-0.619,0.801-1.03c1.154-1.63,2.027-3.423,2.632-5.304c1.086-0.335,1.886-1.338,1.886-2.53v-3.546
-	c0-0.78-0.347-1.477-0.886-1.965v-5.126c0,0,1.053-7.977-9.75-7.977s-9.75,7.977-9.75,7.977v5.126
-	c-0.54,0.488-0.886,1.185-0.886,1.965v3.546c0,0.934,0.491,1.756,1.226,2.231c0.886,3.857,3.206,6.633,3.206,6.633v3.24
-	C20.296,39.899,19.65,40.986,18.613,41.552z"></path>
+  c6.454,0,12.367-2.31,16.964-6.144c-0.424-0.358-0.884-0.68-1.394-0.934l-8.467-4.233c-1.094-0.547-1.785-1.665-1.785-2.888v-3.322
+  c0.238-0.271,0.51-0.619,0.801-1.03c1.154-1.63,2.027-3.423,2.632-5.304c1.086-0.335,1.886-1.338,1.886-2.53v-3.546
+  c0-0.78-0.347-1.477-0.886-1.965v-5.126c0,0,1.053-7.977-9.75-7.977s-9.75,7.977-9.75,7.977v5.126
+  c-0.54,0.488-0.886,1.185-0.886,1.965v3.546c0,0.934,0.491,1.756,1.226,2.231c0.886,3.857,3.206,6.633,3.206,6.633v3.24
+  C20.296,39.899,19.65,40.986,18.613,41.552z"></path>
               <g>
                 <path style="fill: #556080" d="M26.953,0.004C12.32-0.246,0.254,11.414,0.004,26.047C-0.138,34.344,3.56,41.801,9.448,46.76
-		c0.385-0.336,0.798-0.644,1.257-0.894l7.907-4.313c1.037-0.566,1.683-1.653,1.683-2.835v-3.24c0,0-2.321-2.776-3.206-6.633
-		c-0.734-0.475-1.226-1.296-1.226-2.231v-3.546c0-0.78,0.347-1.477,0.886-1.965v-5.126c0,0-1.053-7.977,9.75-7.977
-		s9.75,7.977,9.75,7.977v5.126c0.54,0.488,0.886,1.185,0.886,1.965v3.546c0,1.192-0.8,2.195-1.886,2.53
-		c-0.605,1.881-1.478,3.674-2.632,5.304c-0.291,0.411-0.563,0.759-0.801,1.03V38.8c0,1.223,0.691,2.342,1.785,2.888l8.467,4.233
-		c0.508,0.254,0.967,0.575,1.39,0.932c5.71-4.762,9.399-11.882,9.536-19.9C53.246,12.32,41.587,0.254,26.953,0.004z">
+    c0.385-0.336,0.798-0.644,1.257-0.894l7.907-4.313c1.037-0.566,1.683-1.653,1.683-2.835v-3.24c0,0-2.321-2.776-3.206-6.633
+    c-0.734-0.475-1.226-1.296-1.226-2.231v-3.546c0-0.78,0.347-1.477,0.886-1.965v-5.126c0,0-1.053-7.977,9.75-7.977
+    s9.75,7.977,9.75,7.977v5.126c0.54,0.488,0.886,1.185,0.886,1.965v3.546c0,1.192-0.8,2.195-1.886,2.53
+    c-0.605,1.881-1.478,3.674-2.632,5.304c-0.291,0.411-0.563,0.759-0.801,1.03V38.8c0,1.223,0.691,2.342,1.785,2.888l8.467,4.233
+    c0.508,0.254,0.967,0.575,1.39,0.932c5.71-4.762,9.399-11.882,9.536-19.9C53.246,12.32,41.587,0.254,26.953,0.004z">
                 </path>
               </g>
             </svg></span><span class="u-file-icon u-icon u-icon-2" data-href="Contact.html#carousel_3768"><img
@@ -267,7 +267,7 @@
       </div>
     </div>
   </header>
-  
+
   <section class="u-clearfix u-custom-color-2 u-lightbox u-section-1" id="sec-39e6">
     <div class="u-clearfix u-sheet u-sheet-1">
       <!--products--><!--products_options_json--><!--{"type":"Recent","source":"","tags":"","count":""}--><!--/products_options_json-->
@@ -277,32 +277,37 @@
         <div class="u-repeater u-repeater-1">
 
 
-<script>
-  function setProductInLocalStorage(product) {
-    console.log(product);
-    console.log('setProductInLocalStorage');
-    localStorage.setItem("products", JSON.stringify(product));
+          <script>
+            function setProductInLocalStorage(product) {
+              console.log(product);
+              console.log('setProductInLocalStorage');
+              localStorage.setItem("product", JSON.stringify(product));
+              setTimeout(() => {
+                window.location.href = 'product-page.php'
+              }, 200);
 
-  }
-  
-</script>
-        <!-- PHP  -->
-        <?php 
-  
-  // Read the JSON file  
-  $json = file_get_contents('prods.json'); 
-    echo gettype($json);
-  // Decode the JSON file 
-  $json_data = json_decode($json,true); 
-  // Example: Loop through each item
-  foreach ($json_data as $item) {
-  echo '  <div class="u-align-center u-container-style u-products-item u-repeater-item">
+            }
+
+          </script>
+          <!-- PHP  -->
+          <?php
+
+          // Read the JSON file  
+          $json = file_get_contents('prods.json');
+          echo gettype($json);
+          // Decode the JSON file 
+          $json_data = json_decode($json, true);
+          // Example: Loop through each item
+          foreach ($json_data as $item) {
+            $productJson = htmlspecialchars(json_encode($item), ENT_QUOTES, 'UTF-8');
+
+            echo '  <div class="u-align-center u-container-style u-products-item u-repeater-item">
               <div class="u-container-layout u-similar-container u-container-layout-1">
                 <img class="u-border-2 u-border-grey-75 u-image u-image-default u-image-1"
-                  src=" '.$item["image"].'" alt="" data-image-width="1280"
+                  src=" ' . $item["image"] . '" alt="" data-image-width="1280"
                   data-image-height="960" data-animation-out="0" />
                 <p class="u-align-center u-text u-text-default u-text-1">
-                  '.$item["name"].'
+                  ' . $item["name"] . '
                 </p>
                 <!--product_price-->
                 <div class="u-product-control u-product-price u-product-price-1" data-add-zero-cents="false">
@@ -313,20 +318,20 @@
                     </div>
                     <!--/product_old_price--><!--product_regular_price-->
                     <div class="u-price" style="font-size: 1.25rem; font-weight: 700">
-                      <!--product_regular_price_content--> '.$item["price"].' <!--/product_regular_price_content-->
+                      <!--product_regular_price_content--> ' . $item["price"] . ' <!--/product_regular_price_content-->
                     </div>
                     <!--/product_regular_price-->
                   </div>
                 </div>
                 <!--/product_price-->
-                <a click="setProductInLocalStorage('.$item["name"].')"
-                  class="u-border-none u-btn u-button-style u-grey-90 u-hover-grey-40 u-btn-1">Achat</a>
-              </div>
-            </div>' ;
-  }
-    
-  ?>
-        
+                  <button onclick=\'setProductInLocalStorage(' . $productJson . ')\'
+          class="u-border-none u-btn u-button-style u-grey-90 u-hover-grey-40 u-btn-1">Achat</button>
+       </div>
+            </div>';
+          }
+
+          ?>
+
           <!--/product_item-->
         </div>
         <div class="u-list-control"></div>
@@ -335,7 +340,7 @@
     </div>
   </section>
 
-  
+
 
   <footer class="u-align-center u-clearfix u-custom-color-2 u-footer u-footer" id="sec-ab3f">
     <div class="u-clearfix u-sheet u-sheet-1">
---
+++ product-page.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html style="font-size: 16px" lang="fr">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Product Page</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+    <link rel="stylesheet" href="product-page.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <link rel="stylesheet" href="femme.css" media="screen" />
+    <meta name="generator" content="Nicepage 4.21.5, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster:400">
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            "name": "Copie de Site1",
+            "logo": "images/logoPNG.png",
+            "sameAs": [
+                "https://www.facebook.com/profile.php?id=61552121878099&sk=about",
+                "https://www.instagram.com/accessoiresrg/",
+                "https://www.tiktok.com/@rg.accessoires6"
+            ]
+        }
+    </script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Product Page">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+
+    <script>
+        function getProductFromLocalStorage() {
+            const product = JSON.parse(localStorage.getItem("product"));
+            console.log(product);
+            return product;
+        }
+
+        function displayProduct() {
+            const product = getProductFromLocalStorage();
+            if (product) {
+                const productContainer = document.getElementById("product-container");
+                productContainer.innerHTML = `
+                <div class="u-align-center u-container-style u-products-item u-repeater-item">
+                    <div class="u-container-layout u-similar-container u-container-layout-1">
+                        <img class="u-border-2 u-border-grey-75 u-image u-image-default u-image-1"
+                            src="${product.image}" alt="" data-image-width="1280"
+                            data-image-height="960" data-animation-out="0" />
+                        <p class="u-align-center u-text u-text-default u-text-1">
+                            ${product.name}
+                        </p>
+                        <div class="u-product-control u-product-price u-product-price-1" data-add-zero-cents="false">
+                            <div class="u-price-wrapper">
+                                <div class="u-old-price" style="text-decoration: line-through !important">
+                                    $20.00
+                                </div>
+                                <div class="u-price" style="font-size: 1.25rem; font-weight: 700">
+                                    ${product.price}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+            }
+        }
+        document.addEventListener("DOMContentLoaded", displayProduct);
+
+    </script>
+</head>
+
+<body class="u-body u-xl-mode">
+    <div id="product-container">zzzzzzzz</div>
+
+</body>
+
+</html>
